d878f931a432693a534edcf6160883db87d05623
johannes67890 <johannes@orager.dk>
2024-10-25 22:31:16 +0200

    Refactor file structure and add image duplication detection

--- src/init.py
+++ src/init.py
@@ -7,9 +7,10 @@ from glob import glob
 from multiprocessing import Pool, cpu_count
 import shutil  # For moving files
 import dub
-import threading
 import blurry
 import util
+import threading
+import canvas
 
 def list_images(directory, valid_exts=(".jpg", ".jpeg", ".png", ".bmp", ".nef")):
     image_paths = []
@@ -26,6 +27,7 @@ def load_next_chunk(image_paths, chunk_size, current_index, result_list: list, o
     with Pool(processes=cpu_count()) as pool:
             results = pool.starmap(blurry.process_image, [(imagePath, output_format, threshold, max_width, max_height) for imagePath in next_chunk])
 
+
     result_list.clear()
     result_list.extend(results)
 
@@ -36,7 +38,7 @@ if __name__ == '__main__':
     ap.add_argument("-t", "--threshold", type=float, default=150.0, help="focus measures that fall below this value will be considered 'blurry'")
     ap.add_argument("--format", type=str, default="png", choices=["png", "jpg"], help="desired output image format")
     ap.add_argument("--max-width", type=int, default=1720, help="maximum width of the displayed image")
-    ap.add_argument("--max-height", type=int, default=1100, help="maximum height of the displayed image")
+    ap.add_argument("--max-height", type=int, default=1000, help="maximum height of the displayed image")
     ap.add_argument("--chunk-size", type=int, default=15, help="number of images to process at a time")
     args = vars(ap.parse_args())
 
@@ -48,6 +50,10 @@ if __name__ == '__main__':
     current_index = 0  # To keep track of the current index in image_paths
     # Load the first chunk of images
     load_next_chunk(image_paths, args["chunk_size"], current_index, result_list, args["format"], args["threshold"], args["max_width"], args["max_height"])
+
+     # Start a background thread to preload the next chunks
+    preload_thread = threading.Thread(target=load_next_chunk, args=(image_paths, args["chunk_size"], current_index + args["chunk_size"], result_list, args["format"], args["threshold"], args["max_width"], args["max_height"]))
+    preload_thread.start()
 
     delete_dir = "deleted_images"
     os.makedirs(delete_dir, exist_ok=True)
@@ -66,6 +72,7 @@ if __name__ == '__main__':
             # Get the corresponding image path for saving/deleting
             image_path = image_paths[current_index + idx]
 
+            # Add the date to the image
             date = util.get_image_metadata_date(image_path)
             (text_width, text_height), baseline = cv2.getTextSize(date, cv2.FONT_ITALIC, 0.8, 2)
             text_x = 10  # Padding from left
@@ -73,7 +80,6 @@ if __name__ == '__main__':
             cv2.rectangle(resized_image, (text_x - 5, text_y - text_height - baseline), (text_x + text_width + 5, text_y + 5), (255, 255, 255), -1)
             cv2.putText(resized_image, f"{date}", (text_x, text_y), cv2.FONT_ITALIC, 0.8, (0, 0, 0), 2)
 
-            # Show the image with blur information
             cv2.imshow("Image", resized_image)
             print(f"Focus Measure: {fm:.2f} - {text}")
 
